cae38d5e23405afc93225d796f6ebec72575becc
Elisabeth 'TerraNova' Schulz <elisabeth.schulz@codecentric.de>
2023-11-06 16:12:55 +0100

    Infrastructure & setup

---
+++ src/day1.rs
@@ -0,0 +1,42 @@
+use nom::bytes::complete::tag;
+use nom::character::complete::digit1;
+use nom::combinator::{map, map_res};
+use nom::IResult;
+use nom::multi::{many1, separated_list1};
+use nom::sequence::terminated;
+
+
+fn food_list(input: &str) -> IResult<&str, usize> {
+    map(
+        many1(terminated(map_res(digit1, str::parse), tag("\n"))),
+        |list: Vec<usize>| list.into_iter().sum(),
+    )(input)
+}
+
+fn parse(input: String) -> Vec<usize> {
+    let (_, result) = separated_list1(tag("\n"), food_list)(&input).expect("parse ok");
+    result
+}
+
+fn part1(input: &Vec<usize>) -> usize {
+    input.iter().cloned().max().unwrap_or(0usize)
+}
+
+fn part2(input: &Vec<usize>) -> usize {
+    let mut buffer = [0usize; 4];
+
+    for next in input {
+        let next = *next;
+        buffer[0] = next;
+        let mut idx = 0;
+        while idx < 3 && buffer[idx] > buffer[idx + 1] {
+            buffer.swap(idx, idx + 1);
+            idx += 1;
+        }
+    }
+    buffer[1..].iter().cloned().sum()
+}
+
+pub fn solve() {
+    crate::solve("day1", parse, part1, part2)
+}
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use std::fmt::Display;
+use std::fs;
+use std::time::Instant;
+
+fn solve<
+    Intermediate,
+    Result : Display,
+    Parse: FnOnce(String) -> Intermediate,
+    Part1: FnOnce(&Intermediate) -> Result,
+    Part2: FnOnce(&Intermediate) -> Result>(
+    filename: &str,
+    parse: Parse,
+    solve_part_1: Part1,
+    solve_part_2: Part2
+){
+    let path = format!("inputfiles/{}", filename);
+    let contents = fs::read(path).expect("Input file readable");
+    let input = String::from_utf8(contents).expect("valid utf8");
+    let prepared = parse(input);
+
+    let start = Instant::now();
+    let solution_part1 = solve_part_1(&prepared);
+    let after_p1 = Instant::now();
+    let solution_part2 = solve_part_2(&prepared);
+    let after_p2 = Instant::now();
+
+    println!("Solved {}, part1: {}, part2: {} ({:?} part 1, {:?} part 2)",
+        filename,
+        solution_part1,
+        solution_part2,
+        after_p1 - start,
+        after_p2 - after_p1
+    )
+}
+
+
+mod day1;
+
+
+fn main() {
+    let day_pointers = vec![
+        day1::solve
+    ];
+
+    let start = Instant::now();
+    for ptr in day_pointers {
+        ptr()
+    }
+    println!("AoC so far, including io: {:?}", Instant::now() - start)
+}
