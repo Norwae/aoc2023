dad4595fc362a56ae2a86ae96671778612d4856e
Elisabeth 'TerraNova' Schulz <elisabeth.schulz@codecentric.de>
2023-11-23 14:48:02 +0100

    Create all the skellingtons

--- src/main.rs
+++ src/main.rs
@@ -105,11 +105,36 @@ fn no_part_2<T: ?Sized>(_input: &T) -> &'static str {
 
 mod day1;
 mod day2;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
+mod day9;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
+mod day17;
+mod day18;
+mod day19;
+mod day20;
+mod day21;
+mod day22;
+mod day23;
+mod day24;
 
 
 fn main() {
     let day_pointers = vec![
-        day1::solve, day2::solve
+        day1::solve, day2::solve, day3::solve, day4::solve, day5::solve, day6::solve,
+        day7::solve, day8::solve, day9::solve, day10::solve, day11::solve, day12::solve,
+        day13::solve, day14::solve, day15::solve, day16::solve, day17::solve, day18::solve,
+        day19::solve, day20::solve, day21::solve, day22::solve, day23::solve, day24::solve,
     ];
 
     let mut context = Context::default();
