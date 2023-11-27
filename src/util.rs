deb3be20ebc798b900eb8f9b5f78961a8e5b360f
Elisabeth 'TerraNova' Schulz <elisabeth.schulz@codecentric.de>
2023-11-27 13:15:47 +0100

    Consume one, and only one

--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,10 @@
-use nom::character::complete::{digit1, multispace1};
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::character::complete::{digit1, multispace1, space1};
 use nom::combinator::map_res;
 use nom::IResult;
 use nom::sequence::terminated;
 
 pub fn parse_u64(input: &str) -> IResult<&str, u64> {
-    terminated(map_res(digit1, str::parse), multispace1)(input)
+    terminated(map_res(digit1, str::parse), alt((space1, tag("\n"))))(input)
 }
