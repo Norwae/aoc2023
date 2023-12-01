d5a453b348c2899d6467078daebd1b4714dfae0b
Elisabeth 'TerraNova' Schulz <elisabeth.schulz@codecentric.de>
2023-12-01 11:24:02 +0100

    use buildin u64

--- src/util.rs
+++ src/util.rs
@@ -1,12 +1,8 @@
 use nom::branch::alt;
-use nom::character::complete::{digit1, line_ending, space1};
-use nom::combinator::map_res;
+use nom::character::complete::{line_ending, space1, u64 as parse_u64};
 use nom::IResult;
 use nom::sequence::terminated;
 
-pub fn parse_u64(input: &str) -> IResult<&str, u64> {
-    map_res(digit1, str::parse)(input)
-}
 pub fn parse_u64_terminated(input: &str) -> IResult<&str, u64> {
     terminated(parse_u64, alt((space1, line_ending)))(input)
 }
