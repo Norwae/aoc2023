97e255a1e0548f8633f745cfa70aa66d1651c48c
Elisabeth 'TerraNova' Schulz <elisabeth.schulz@codecentric.de>
2023-12-04 09:46:25 +0100

    Optimize some parsers

--- src/day4.rs
+++ src/day4.rs
@@ -1,26 +1,18 @@
 use nom::bytes::complete::{tag, take, take_until};
 use nom::character::complete::{line_ending, space1, u64 as parse_u64};
-use nom::combinator::map;
+use nom::combinator::{flat_map, map};
 use nom::IResult;
-use nom::multi::{many1, separated_list1};
+use nom::multi::{fold_many1, many1, separated_list1};
 use nom::sequence::{preceded, tuple};
 
 #[derive(Debug, Default)]
 struct Card {
-    my_numbers: Vec<u64>,
-    chosen_numbers: Vec<u64>
+    match_count: u64
 }
 
 impl Card {
-
-    fn match_count(&self) -> usize {
-        let Self{my_numbers, chosen_numbers} = self;
-
-        my_numbers.iter().filter(|n| chosen_numbers.contains(n)).count()
-    }
-
     fn value(&self) -> u64 {
-        let count = self.match_count();
+        let count = self.match_count;
         if count > 0 {
             1 << (count - 1)
         } else {
@@ -35,15 +27,20 @@ fn u64_with_sep(input: &str) -> IResult<&str, u64> {
 
 
 fn one_card(input: &str) -> IResult<&str, Card> {
-    map(tuple((
+    map(flat_map(tuple((
         take_until(": "),
         take(1usize),
         many1(u64_with_sep),
-        tag(" |"),
-        many1(u64_with_sep)
-    )), |(_, _, my_numbers, _, chosen_numbers)| {
-        Card { my_numbers, chosen_numbers }
-    })(input)
+        tag(" |")
+    )), |(_, _, my_numbers, _)| {
+        fold_many1(u64_with_sep, ||0u64, move |sum: u64,next: u64|{
+            if my_numbers.contains(&next) {
+                sum + 1
+            } else {
+                sum
+            }
+        })
+    }), |match_count|Card {match_count})(input)
 }
 
 fn parse(input: &str) -> IResult<&str, Vec<Card>> {
@@ -59,7 +56,7 @@ fn part2(input: &Vec<Card>) -> u64 {
 
     for i in 0..input.len() {
         let (additional_cards, copies) = input[i];
-        let additional_cards = additional_cards.match_count();
+        let additional_cards = additional_cards.match_count as usize;
 
         let lower = i + 1;
         let higher = (i + additional_cards).min(input.len() - 1);
