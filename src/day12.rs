3f1272966cb1b04abe8b92cd4a18e37e2bd0139b
Elisabeth 'TerraNova' Schulz <schulz.elisabeth.terra@gmail.com>
2023-12-12 22:09:14 +0100

    Space / time tradeoff does not help us either

--- src/day12.rs
+++ src/day12.rs
@@ -2,7 +2,6 @@ use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::{line_ending, space1, u32 as parse_u32};
 use nom::combinator::{map, value};
-use nom::error::dbg_dmp;
 use nom::IResult;
 use nom::multi::{many1, separated_list1};
 use nom::sequence::tuple;
@@ -46,12 +45,54 @@ fn generate_gaps(to_distribute: usize, gaps: &mut Vec<usize>, offset: usize, tar
     }
 }
 
+fn match_mask_with_partial(state: &[SpringState], gaps: &Vec<usize>, damaged: &Vec<usize>, assigned_count: usize) -> bool {
+    let mut gaps = gaps.clone();
+    let mut offset = 0;
+    gaps.push(state.len() - assigned_count);
+
+    for (damaged, gap) in damaged.iter().zip(gaps.iter()) {
+        let damaged = *damaged;
+        let gap = *gap;
+
+        for _ in 0..damaged {
+            if state[offset] == OPERATIONAL {
+                return false
+            }
+            offset += 1
+        }
+
+        for _ in 0..gap {
+            if state[offset] == DAMAGED {
+                return false
+            }
+            offset += 1
+        }
+    }
+
+    true
+}
+
 impl Problem {
     fn mask_matches(&self, mask: &Mask) -> usize {
-        for offset in 0..=(self.states.len() - mask.width) {
-
+        let assigned_count = mask.width + self.broken_required;
+        let mut sum = 0;
+        for offset in 0..=(self.states.len() - assigned_count) {
+            match self.states[offset] {
+                OPERATIONAL => (), // not a match here, so we can advance right now
+                DAMAGED => { // we anchor here, can go no further, so break the loop after
+                    if match_mask_with_partial(&self.states[offset..], &mask.gaps, &self.broken_series, assigned_count) {
+                        sum += 1
+                    }
+                    break;
+                }
+                UNKNOWN => { // we can start here... but we don't need to anchor
+                    if match_mask_with_partial(&self.states[offset..], &mask.gaps, &self.broken_series, assigned_count) {
+                        sum += 1
+                    }
+                }
+            }
         }
-        1
+        sum
     }
 
     fn generate_masks(&self) -> Vec<Mask> {
@@ -74,12 +115,32 @@ impl Problem {
 
     fn possible_variants(&self) -> usize {
         let mut masks = self.generate_masks();
+        println!("{}", masks.len());
 
         masks.into_iter().map(|mask| {
             self.mask_matches(&mask)
         }).sum()
     }
+
+    fn unfold(&self) -> Self {
+        let mut states = self.states.clone();
+        let mut broken_series = self.broken_series.clone();
+        let mut extend_states = Vec::new();
+        extend_states.push(UNKNOWN);
+        extend_states.extend_from_slice(states.as_slice());
+
+        for _ in 0..4 {
+            states.extend_from_slice(extend_states.as_slice());
+            broken_series.extend_from_slice(self.broken_series.as_slice())
+        }
+        let broken_required = self.broken_required * 5;
+
+        Self {
+            states, broken_series, broken_required
+        }
+    }
 }
+
 
 fn parse_state(input: &str) -> IResult<&str, SpringState> {
     alt((
@@ -110,8 +171,11 @@ fn parse(input: &str) -> IResult<&str, Vec<Problem>> {
 
 
 fn part_1(input: &Vec<Problem>) -> usize {
-    input[0].possible_variants()
+    input.iter().map(Problem::possible_variants).sum()
 }
 
+fn part_2(input: &Vec<Problem>) -> usize {
+    input.iter().map(Problem::unfold).map(|it|it.possible_variants()).sum()
+}
 
-solution!(parse, part_1);
+solution!(parse, part_1, part_2);
