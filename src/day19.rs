856b4493ceac3fdc67d4c16b98a40e27fea09aa3
Elisabeth 'TerraNova' Schulz <elisabeth.schulz@codecentric.de>
2023-12-19 11:58:10 +0100

    Part 2 (range splitting approach)

--- src/day19.rs
+++ src/day19.rs
@@ -1,7 +1,6 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::hash::Hash;
 use std::mem::swap;
-use std::ops::RangeInclusive;
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::{alpha1, line_ending, u32};
@@ -9,7 +8,6 @@ use nom::combinator::{map, value};
 use nom::IResult;
 use nom::multi::{separated_list0, separated_list1};
 use nom::sequence::tuple;
-use crate::day19::Decision::Forward;
 
 #[derive(Debug, Clone)]
 struct Part {
@@ -41,16 +39,6 @@ enum Decision {
     Forward(String),
 }
 
-impl Decision {
-    fn is_terminal(&self) -> bool {
-        match self {
-            Decision::Accept => true,
-            Decision::Reject => true,
-            Forward(_) => false
-        }
-    }
-}
-
 #[derive(Debug, Clone)]
 struct Condition {
     field: ConditionRef,
@@ -80,12 +68,102 @@ struct Rule {
     conditionals: Vec<Condition>,
     default: Decision,
 }
+#[derive(Debug, Clone)]
+struct InputRange {
+    from_x: u32,
+    to_x: u32,
+    from_m: u32,
+    to_m: u32,
+    from_a: u32,
+    to_a: u32,
+    from_s: u32,
+    to_s: u32,
+}
 
-impl Rule {
-    fn is_terminal(&self) -> bool {
-        self.default.is_terminal() && self.conditionals.iter().all(|c|c.destination.is_terminal())
+impl Default for InputRange {
+    fn default() -> Self {
+        InputRange {
+            from_x: 1,
+            to_x: 4001,
+            from_m: 1,
+            to_m: 4001,
+            from_a: 1,
+            to_a: 4001,
+            from_s: 1,
+            to_s: 4001,
+        }
+    }
+}
+
+
+impl InputRange {
+
+    fn len(&self) -> usize {
+        let mut product = 0usize;
+        if !self.is_empty() {
+            product = (self.to_x - self.from_x) as usize;
+            product *= (self.to_m - self.from_m) as usize;
+            product *= (self.to_a - self.from_a) as usize;
+            product *= (self.to_s - self.from_s) as usize;
+        }
+
+        product
+    }
+    fn is_empty(&self) -> bool {
+        self.from_x >= self.to_x || self.from_m >= self.to_m || self.from_a >= self.to_a || self.from_s >= self.to_s
     }
 
+    fn split_for_condition(&self, c: &Condition) -> (InputRange, InputRange) {
+        let mut _then = self.clone();
+        let mut _else = self.clone();
+
+        if let ConditionOp::LT = c.op {
+            match c.field {
+                ConditionRef::X => {
+                    _then.to_x = c.value;
+                    _else.from_x = c.value;
+                }
+                ConditionRef::M => {
+                    _then.to_m = c.value;
+                    _else.from_m = c.value;
+                }
+                ConditionRef::A => {
+                    _then.to_a = c.value;
+                    _else.from_a = c.value;
+                }
+                ConditionRef::S => {
+                    _then.to_s = c.value;
+                    _else.from_s = c.value;
+                }
+            }
+        } else {
+            let value = c.value + 1;
+            match c.field {
+                ConditionRef::X => {
+                    _then.from_x = value;
+                    _else.to_x = value;
+                }
+                ConditionRef::M => {
+                    _then.from_m = value;
+                    _else.to_m = value;
+                }
+                ConditionRef::A => {
+                    _then.from_a = value;
+                    _else.to_a = value;
+                }
+                ConditionRef::S => {
+                    _then.from_s = value;
+                    _else.to_s = value;
+                }
+            }
+        }
+
+        (_then, _else)
+    }
+}
+
+
+impl Rule {
     fn simplify(&mut self) {
         let mut tmp = Vec::new();
         swap(&mut self.conditionals, &mut tmp);
@@ -127,13 +205,39 @@ impl Input {
             match self.apply_rule(rule_label, input) {
                 Decision::Accept => return true,
                 Decision::Reject => return false,
-                Forward(next) => rule_label = next,
+                Decision::Forward(next) => rule_label = next,
             }
         }
     }
 
-    fn find_terminal_rule(&self) -> Option<(&String, &Rule)> {
-        self.ruleset.iter().find(|(_, r)| r.is_terminal())
+    fn apply_input_range_to<'a>(&'a self, range: InputRange, label: &'a str, mut acceptor: impl FnMut(InputRange)) {
+        let mut queue = VecDeque::from([(label, range)]);
+
+        while let Some((label, mut range)) = queue.pop_front() {
+            let rule = &self.ruleset[label];
+            for condition in &rule.conditionals {
+                let (_then, _else) = range.split_for_condition(condition);
+                if !_then.is_empty() {
+                    match &condition.destination {
+                        Decision::Accept => acceptor(_then),
+                        Decision::Reject => (),
+                        Decision::Forward(label) => {
+                           queue.push_back((label, _then))
+                        }
+                    }
+                }
+
+                range = _else
+            }
+
+            match &rule.default {
+                Decision::Accept => acceptor(range),
+                Decision::Reject => (),
+                Decision::Forward(label) => {
+                    queue.push_back((label, range))
+                }
+            }
+        }
     }
 }
 
@@ -157,7 +261,7 @@ fn parse_decision(input: &str) -> IResult<&str, Decision> {
     alt((
         value(Decision::Accept, tag("A")),
         value(Decision::Reject, tag("R")),
-        map(alpha1, |str: &str| Forward(str.to_owned()))
+        map(alpha1, |str: &str| Decision::Forward(str.to_owned()))
     ))(input)
 }
 
@@ -233,13 +337,13 @@ fn part1(input: &Input) -> u32 {
     sum
 }
 
-fn part2(input: &Input) -> u32 {
-    let mut clone = input.find_terminal_rule().unwrap().1.clone();
-    dbg!(&clone);
-    clone.simplify();
-    dbg!(&clone);
+fn part2(input: &Input) -> usize {
+    let mut sum = 0;
+    input.apply_input_range_to(InputRange::default(), "in", |r|{
+        sum += r.len();
+    });
 
-    42
+    sum
 }
 
 nom_solution!(parse, part1, part2);
