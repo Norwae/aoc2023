0dba61cc390a1f33224bad55e1d0a671906a02d0
Elisabeth 'TerraNova' Schulz <elisabeth.schulz@codecentric.de>
2023-12-20 12:16:58 +0100

    Part 1, naive part 2

--- src/day20.rs
+++ src/day20.rs
@@ -6,7 +6,7 @@ use nom::combinator::{map, value};
 use nom::IResult;
 use nom::multi::{separated_list0, separated_list1};
 use nom::sequence::tuple;
-use crate::day20::Module::{Conjunction, FlipFlop};
+use crate::day20::Module::{Broadcaster, Conjunction, FlipFlop};
 
 #[derive(Debug, Clone)]
 enum Module {
@@ -21,97 +21,87 @@ enum Module {
 
 
 #[derive(Debug, Clone)]
-struct PlannedWiring<'a> {
-    label: &'a str,
-    module: Module,
-    outputs: Vec<&'a str>,
-}
-
-#[derive(Debug, Clone)]
 struct WiredModule {
+    label: String,
     module: Module,
     wires: Vec<Wire>,
 }
 
-impl WiredModule {
+#[derive(Debug)]
+struct Pulse {
+    emitter: String,
+    receiver: String,
+    to_slot: usize,
+    high: bool
+}
 
-    fn process_pulse(&mut self, pulse: Pulse, emitted_target: &mut VecDeque<Pulse>) {
-        let Pulse { high, input_port, ..} = pulse;
-        match &mut self.module {
-            Module::Broadcaster => {
-                for wire in self.wires.iter_mut() {
-                    emitted_target.push_back(Pulse {
-                        to_module: wire.to,
-                        input_port: wire.inbound_index,
-                        high,
-                    })
-                }
-            }
-            FlipFlop { memory } => {
-                if !high {
-                    *memory = !*memory;
+impl WiredModule {
+        fn process_pulse(&mut self, high: bool, to_slot: usize, mut enqueue: impl FnMut(Pulse)) {
+            match &mut self.module {
+                Broadcaster => {
                     for wire in self.wires.iter_mut() {
-                        emitted_target.push_back(Pulse {
-                            to_module: wire.to,
-                            input_port: wire.inbound_index,
-                            high: *memory,
+                        enqueue(Pulse {
+                            emitter: self.label.clone(),
+                            receiver: wire.to.clone(),
+                            to_slot: wire.inbound_index,
+                            high
+                        })
+                    }
+                }
+                FlipFlop { memory } => {
+                    if !high {
+                        *memory = !*memory;
+                        for wire in self.wires.iter_mut() {
+                            enqueue(Pulse {
+                                emitter: self.label.clone(),
+                                receiver: wire.to.clone(),
+                                to_slot: wire.inbound_index,
+                                high: *memory,
+                            })
+                        }
+                    }
+                }
+                Conjunction { latest_inputs } => {
+                    latest_inputs[to_slot] = high;
+                    let all_high = latest_inputs.iter().all(|v|*v);
+
+                    for wire in self.wires.iter_mut() {
+                        enqueue(Pulse {
+                            emitter: self.label.clone(),
+                            receiver: wire.to.clone(),
+                            to_slot: wire.inbound_index,
+                            high: !all_high,
                         })
                     }
                 }
             }
-            Conjunction { latest_inputs } => {
-                latest_inputs[input_port] = high;
-                let all_high = latest_inputs.iter().all(|v|*v);
-
-                for wire in self.wires.iter_mut() {
-                    emitted_target.push_back(Pulse {
-                        to_module: wire.to,
-                        input_port: wire.inbound_index,
-                        high: !all_high,
-                    })
-                }
-            }
         }
-    }
 }
 
 #[derive(Debug, Clone)]
 struct Wire {
-    to: usize,
+    to: String,
     inbound_index: usize,
 }
 
 #[derive(Debug, Clone)]
 struct Input {
-    broadcaster_index: usize,
-    modules: Vec<WiredModule>,
+    modules: HashMap<String, WiredModule>,
 }
 
 impl Input {
-    fn push_button(&mut self) -> (usize, usize) {
-        let mut low = 0;
-        let mut high = 0;
-        let mut queue = VecDeque::from([Pulse { to_module: self.broadcaster_index, input_port: 0usize, high: false }]);
 
-        while let Some(next) = queue.pop_back() {
-            if next.high {
-                high += 1;
-            } else {
-                low += 1;
+        fn push_button(&mut self, mut on_pulse: impl FnMut(&Pulse)) {
+            let mut queue = VecDeque::from([Pulse { emitter: "button".to_string(), receiver: "broadcaster".to_string(), to_slot: 0, high: false }]);
+
+            while let Some(next) = queue.pop_front() {
+                on_pulse(&next);
+                let handling_module = self.modules.get_mut(&next.receiver);
+                if let Some(wm) = handling_module  {
+                    wm.process_pulse(next.high, next.to_slot, |p| queue.push_back(p))
+                }
             }
-            let wired_module = &mut self.modules[next.to_module];
-            wired_module.process_pulse(next, &mut queue);
         }
-
-        (low, high)
-    }
-}
-
-#[derive(Debug)]
-struct Pulse {
-    to_module: usize,
-    input_port: usize,
-    high: bool
 }
 
 fn parse_broadcaster(input: &str) -> IResult<&str, (Module, &str)> {
@@ -140,78 +130,87 @@ fn parse_module(input: &str) -> IResult<&str, (Module, &str)> {
     ))(input)
 }
 
-fn parse_planned_wiring(input: &str) -> IResult<&str, PlannedWiring> {
+fn parse_wired(input: &str) -> IResult<&str, (&str, Module, Vec<&str>)> {
     map(tuple((
         parse_module,
         tag(" -> "),
         separated_list0(tag(", "), alpha1)
     )), |((module, label), _, outputs)| {
-        PlannedWiring { label, module, outputs }
+        (label, module, outputs)
     })(input)
 }
 
-fn assign_module_indices<'a>(input: &Vec<PlannedWiring<'a>>) -> HashMap<&'a str, usize> {
-    let mut result = HashMap::new();
-    for planned in input {
-        let mut len = result.len();
-        result.entry(planned.label).or_insert(len);
-
-        for output in &planned.outputs {
-            len = result.len();
-            result.entry(*output).or_insert(len);
-        }
-    }
-
-    result
-}
-
 fn parse_and_reformat(input: &str) -> IResult<&str, Input> {
-    map(separated_list1(line_ending, parse_planned_wiring), |wirings| {
-        let index_assignment = assign_module_indices(&wirings);
-        let mut inbound_count = vec![0; wirings.len()];
-        let broadcaster_index = index_assignment["broadcaster"];
-        let mut modules: Vec<_> = wirings.into_iter().map(|wiring| {
-            let wires = wiring.outputs.into_iter().filter_map(|target_label| {
-                let to = index_assignment[target_label];
-                if to >= inbound_count.len() {
-                    eprintln!("Undefined module named {} found, ignoring wire", target_label);
-                    None
-                } else {
-                    let index_count_ptr = &mut inbound_count[to];
-                    let inbound_index = *index_count_ptr;
-                    *index_count_ptr += 1;
-                    Some(Wire {
-                        to,
-                        inbound_index,
-                    })
-                }
-            }).collect();
-            WiredModule {
-                module: wiring.module,
-                wires,
-            }
-        }).collect();
+    map(separated_list1(line_ending, parse_wired), |wirings| {
+        let mut modules = HashMap::new();
+        let mut inbound_count = HashMap::new();
 
-        for (i, module) in modules.iter_mut().enumerate() {
-            if let Conjunction { latest_inputs } = &mut module.module {
-                *latest_inputs = vec![false; inbound_count[i]];
+        for (label, module, outputs) in wirings {
+            let mut module = WiredModule {
+                module,
+                label: label.to_string(),
+                wires: Vec::with_capacity(outputs.len()),
+            };
+
+            for output in outputs {
+                let inbound_index = inbound_count.entry(output).or_insert(0usize);
+                module.wires.push(Wire {
+                    to: output.to_string(),
+                    inbound_index: *inbound_index,
+                });
+                *inbound_index += 1
+            }
+
+            modules.insert(label.to_string(), module);
+        }
+
+        for (key, module) in &mut modules {
+            if let Conjunction { latest_inputs} = &mut module.module {
+                *latest_inputs = vec![false; inbound_count[&key[..]]]
             }
         }
 
-        Input {
-            broadcaster_index,
-            modules,
-        }
+        Input { modules }
     })(input)
 }
 
 fn part_1(input: &Input) -> usize {
+    let mut low_count =0;
+    let mut high_count = 0;
     let mut input = input.clone();
-    let (l, r) = (0..1000).map(|_|input.push_button()).fold((0usize, 0usize), |(l0, r0), (l1, r1)| {
-        (l0 + l1, r0 + r1)
-    });
 
-    l * r
+    for _ in 0..1000 {
+        input.push_button(|Pulse{ high, ..}|{
+            if *high {
+                high_count += 1
+            } else {
+                low_count += 1
+            }
+        });
+    }
+
+    low_count * high_count
+}
+
+fn part_2(input: &Input) -> usize {
+    let mut pushes = 0;
+    let mut rx_received = false;
+    let mut input = input.clone();
+    while !rx_received {
+        pushes += 1;
+
+        if (pushes % 10000) == 0 {
+            println!("{}...", pushes)
+        }
+
+        input.push_button(|Pulse{high, receiver, ..}|{
+            if receiver == "rx" && !*high {
+                rx_received = true
+            }
+        })
+    }
+
+    pushes
 }
 
 nom_solution!(parse_and_reformat, part_1);
